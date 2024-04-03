d8b371d6d54e2eac6ff9ce8db61e7208c340ac1f
Alizadekh <aykhanalizadekh@gmail.com>
2024-04-02 23:43:33 -0700

    Add Full Porjecgt

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let screen = document.querySelector("#inputScreen");
 let num = document.querySelectorAll(".num");
-let operation = document.querySelectorAll(".op-btn");
 let equalBtn = document.querySelector(".equal");
 let reset = document.querySelector(".reset");
 
@@ -10,10 +9,51 @@ num.forEach((btn) => {
     screen.value += btn.value;
   });
 });
+
 equalBtn.addEventListener("click", function () {
-  console.log(eval(screen.value));
-  screen.value = eval(screen.value);
+  const input = screen.value;
+
+  const numberPattern = /\d+(\.\d+)?/g;
+  const operatorPattern = /[\+\-\*\/]/g;
+
+  const numbers = input.match(numberPattern).map(Number);
+  const operators = input.match(operatorPattern);
+
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === "*" || operators[i] === "/") {
+      const result =
+        operators[i] === "*"
+          ? numbers[i] * numbers[i + 1]
+          : numbers[i] / numbers[i + 1];
+      numbers.splice(i, 2, result);
+      operators.splice(i, 1);
+      i--;
+    }
+  }
+
+  let result = numbers[0];
+  for (let i = 0; i < operators.length; i++) {
+    const operator = operators[i];
+    const nextNumber = numbers[i + 1];
+
+    switch (operator) {
+      case "+":
+        result += nextNumber;
+        break;
+      case "-":
+        result -= nextNumber;
+        break;
+      case "*":
+        result *= nextNumber;
+        break;
+      case "/":
+        result /= nextNumber;
+        break;
+    }
+  }
+  screen.value = result;
 });
+
 reset.addEventListener("click", function () {
   screen.value = "";
 });
